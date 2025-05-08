a5c5556aac153aa027d57ee5ca54819554cf1d39
Amardeep Singh <amardeepsingh@mac.home>
2025-05-08 15:25:27 -0400

    debugging

--- src/controllers/tenant.controller.js
+++ src/controllers/tenant.controller.js
@@ -15,7 +15,12 @@ const getTenant = catchAsync(async (req, res) => {
 
   logger.debug(`[GET TENANT] tenant data: ${JSON.stringify(tenant, null, 2)}`);
 
-  res.send(tenant.toJSON());
+  try {
+    res.send(tenant.toJSON());
+  } catch (err) {
+    logger.error(`[GET TENANT] Failed to send tenant JSON: ${err.message}`, err);
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Failed to return tenant data');
+  }
 });
 
 const updateTenant = catchAsync(async (req, res) => {
